674537e9c0a165ddb4e01e42b2e293cdcdb66682
Michael Sutton <hicallmesutton@gmail.com>
2023-07-31 21:54:28 -0500

    started building components

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+      <div className="header">
+        <img src="./images/baby_steps_logo-small.png" />
+      </div>
+    );
+  }
+  
+  export default Header;
+  
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,31 @@
+function Home() {
+    return (
+      <div className="container p-4">
+        <h1>Welcome back Michael</h1>
+        <h4 className="mb-4">Current enrolled students</h4>
+        <div className="card p-3 mb-2">
+            <div className="card-body">
+                <h5>Student name goes here</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Daily Log</a>
+            </div>
+        </div>
+        <div className="card p-3 mb-2">
+            <div className="card-body">
+                <h5>Student name goes here</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Daily Log</a>
+            </div>
+        </div>
+        <div className="card p-3 mb-2">
+            <div className="card-body">
+                <h5>Student name goes here</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Daily Log</a>
+            </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Home;
